7480064649d9df26b681a1d674ba17befa2bf441
rap2h <raphaelht@gmail.com>
2019-01-31 15:09:50 +0100

    hop

--- pages/opendata.js
+++ pages/opendata.js
@@ -3,8 +3,6 @@ import Head from "next/head";
 import Layout from "../components/Layout";
 import "../styles/opendata.css";
 
-dsqdqs.dsqdqsdqs.dsqdsdq = dsqdsq;
-
 export default () => (
   <Layout>
     <div className="opendata-view">
